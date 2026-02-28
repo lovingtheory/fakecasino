84d1a640860b07cfb03a32dde409933aa54f7314
lovingtheory <157224257+lovingtheory@users.noreply.github.com>
2026-02-28 07:05:49 +0100

    del api/comments/leaderboard and edit pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Leaderboard from "../components/Leaderboard";
+import Leaderboard from "./components/leaderboard";
 
 export default function Home() {
   const [username, setUsername] = useState("");
